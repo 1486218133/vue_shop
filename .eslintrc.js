ee3264ddff8f6621453ff209abefb11c3fb0f20e
chenmingquan <1486218133@qq.com>
2022-01-05 22:07:12 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'arrow-parens':0,
+    'generator-star-spacing':0,
+    'singleQuote':true,
+    'space-before-function-paren':0
   }
 }
--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -136,7 +136,12 @@
     </el-dialog>
 
     <!-- 修改对话框 -->
-    <el-dialog title="修改用户信息" :visible.sync="changeUser" width="50%" @close='closeChangeUser'>
+    <el-dialog
+      title="修改用户信息"
+      :visible.sync="changeUser"
+      width="50%"
+      @close="closeChangeUser"
+    >
       <!-- 修改用户表单 -->
       <el-form
         :model="changeUserFrom"
@@ -148,10 +153,10 @@
           <el-input v-model="changeUserFrom.username" disabled></el-input>
         </el-form-item>
         <el-form-item label="邮箱" prop="email">
-          <el-input v-model="changeUserFrom.email" ></el-input>
+          <el-input v-model="changeUserFrom.email"></el-input>
         </el-form-item>
         <el-form-item label="手机" prop="mobile">
-          <el-input v-model="changeUserFrom.mobile" ></el-input>
+          <el-input v-model="changeUserFrom.mobile"></el-input>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
@@ -167,27 +172,27 @@ export default {
   data() {
     //   自定义验证规则
     var checkEmail = (rule, value, cb) => {
-      const regEmail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+      const regEmail = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/
       if (regEmail.test(value)) {
-        return cb();
+        return cb()
       }
-      cb(new Error("请输入合法邮箱"));
-    };
+      cb(new Error('请输入合法邮箱'))
+    }
 
     // 手机号验证规则
     var checkMobile = (rule, value, cb) => {
       const regMobile =
-        /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/;
+        /^(13[0-9]|14[01456879]|15[0-35-9]|16[2567]|17[0-8]|18[0-9]|19[0-35-9])\d{8}$/
       if (regMobile.test(value)) {
-        return cb();
+        return cb()
       }
-      cb(new Error("请输入合法手机号"));
-    };
+      cb(new Error('请输入合法手机号'))
+    }
 
     return {
       // 获取数据的参数对象
       getUserList: {
-        query: "",
+        query: '',
         // 当前页码
         pagenum: 1,
         // 每页显示条数
@@ -201,33 +206,33 @@ export default {
       dialogVisible: false,
       //添加用户对象
       addForm: {
-        username: "",
-        password: "",
-        email: "",
-        mobile: "",
+        username: '',
+        password: '',
+        email: '',
+        mobile: '',
       },
       //添加用户表单验证规则
       addrules: {
         username: [
-          { required: true, message: "请输入用户名", trigger: "blur" },
-          { min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur" },
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' },
         ],
         password: [
-          { required: true, message: "请输入密码", trigger: "blur" },
+          { required: true, message: '请输入密码', trigger: 'blur' },
           {
             min: 5,
             max: 10,
-            message: "长度在 5 到 10 个字符",
-            trigger: "blur",
+            message: '长度在 5 到 10 个字符',
+            trigger: 'blur',
           },
         ],
         email: [
-          { required: true, message: "请输入邮箱", trigger: "blur" },
-          { validator: checkEmail, trigger: "blur" },
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' },
         ],
         mobile: [
-          { required: true, message: "请输入手机号", trigger: "blur" },
-          { validator: checkMobile, trigger: "blur" },
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' },
         ],
       },
       //修改用户信息对话框
@@ -235,116 +240,126 @@ export default {
       //修改用户信息的数据
       changeUserFrom: {},
       //修改用户表单的验证规则
-      changeUserFromRules:{
-          email:[
-              { required: true, message: "请输入邮箱", trigger: "blur" },
-              {validator:checkEmail,trigger:'blur'}
-          ],
-          mobile:[
-              { required: true, message: "请输入手机号", trigger: "blur" },
-              {validator:checkMobile,trigger:'blur'}
-          ]
-      }
-    };
+      changeUserFromRules: {
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          { validator: checkEmail, trigger: 'blur' },
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' },
+        ],
+      },
+    }
   },
   created() {
-    this.getUsers();
+    this.getUsers()
   },
   methods: {
     //   获取用户数据
     async getUsers() {
-      const { data: res } = await this.$http.get("users", {
+      const { data: res } = await this.$http.get('users', {
         params: this.getUserList,
-      });
-      if (res.meta.status !== 200) return this.$message.error("获取数据失败");
-      this.userList = res.data.users;
-      this.total = res.data.total;
+      })
+      if (res.meta.status !== 200) return this.$message.error('获取数据失败')
+      this.userList = res.data.users
+      this.total = res.data.total
     },
     // 监听每页数据
     handleSizeChange(newSize) {
-      this.getUserList.pagesize = newSize;
-      this.getUsers();
+      this.getUserList.pagesize = newSize
+      this.getUsers()
     },
     // 监听页码的变化
     handleCurrentChange(newPage) {
-      this.getUserList.pagenum = newPage;
-      this.getUsers();
+      this.getUserList.pagenum = newPage
+      this.getUsers()
     },
     // 修改用户状态
     async userStateChange(newState) {
       const { data: res } = await this.$http.put(
         `users/${newState.id}/state/${newState.mg_state}`
-      );
+      )
       if (res.meta.status !== 200) {
-        newState.mg_state = !newState.mg_state;
-        return this.$message.error("更新数据失败");
+        newState.mg_state = !newState.mg_state
+        return this.$message.error('更新数据失败')
       }
-      this.$message.success("更新数据成功");
+      this.$message.success('更新数据成功')
     },
     // 搜索用户
     searchUser() {
-      this.getUsers();
+      this.getUsers()
     },
     // 关闭对话框重置表单
     shut() {
-      this.$refs.ruleFormRef.resetFields();
+      this.$refs.ruleFormRef.resetFields()
     },
     // 添加用户验证
     addUser() {
       this.$refs.ruleFormRef.validate(async (bool) => {
-        if (!bool) return;
+        if (!bool) return
         // 发起请求添加用户
-        const { data: res } = await this.$http.post("users", this.addForm);
-        if (res.meta.status !== 201) return this.$message.error("添加失败");
-        this.$message.success("添加成功");
-        this.dialogVisible = false;
-        this.getUsers();
-      });
+        const { data: res } = await this.$http.post('users', this.addForm)
+        if (res.meta.status !== 201) return this.$message.error('添加失败')
+        this.$message.success('添加成功')
+        this.dialogVisible = false
+        this.getUsers()
+      })
     },
     // 修改用户信息
     async amend(id) {
       // 发起请求查询数据
-      const { data: res } = await this.$http.get("users/" + id);
-      if (res.meta.status !== 200) return this.$message.error("获取失败");
-      this.changeUserFrom = res.data;
-      this.changeUser = true;
+      const { data: res } = await this.$http.get('users/' + id)
+      if (res.meta.status !== 200) return this.$message.error('获取失败')
+      this.changeUserFrom = res.data
+      this.changeUser = true
     },
     // 关闭修改框清空验证
-    closeChangeUser(){
-        this.$refs.changeUserFromRef.resetFields()
+    closeChangeUser() {
+      this.$refs.changeUserFromRef.resetFields()
     },
     //提交修改信息
-    addChangeUser(){
-        this.$refs.changeUserFromRef.validate(async bool=>{
-            if(!bool) return
-            //发起请求
-            const {data:res}=await this.$http.put('users/'+this.changeUserFrom.id,{email:this.changeUserFrom.email,mobile:this.changeUserFrom.mobile})
-            if(res.meta.status!==200) return this.$message.error('更新数据失败')
-            //关闭对话框
-            this.changeUser=false
-            //刷新数据
-            this.getUsers()
-            //提示修改成功
-            this.$message.success('修改成功')
-        })
+    addChangeUser() {
+      this.$refs.changeUserFromRef.validate(async (bool) => {
+        if (!bool) return
+        //发起请求
+        const { data: res } = await this.$http.put(
+          'users/' + this.changeUserFrom.id,
+          {
+            email: this.changeUserFrom.email,
+            mobile: this.changeUserFrom.mobile,
+          }
+        )
+        if (res.meta.status !== 200) return this.$message.error('更新数据失败')
+        //关闭对话框
+        this.changeUser = false
+        //刷新数据
+        this.getUsers()
+        //提示修改成功
+        this.$message.success('修改成功')
+      })
     },
     //删除用户操作
-    async removeUser(id){
-        const confirmResult=await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+    async removeUser(id) {
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该用户, 是否继续?',
+        '提示',
+        {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
-          type: 'warning'
-        }).catch(err=>err)
-        //如果结果为confirm则表示确定
-        //如果结果为cancel则表示取消
-        if(confirmResult!=="confirm") return this.$message.info('取消删除')
-        const {data:res} = await this.$http.delete('users/'+id)
-        if(res.meta.status!==200) return this.$message.error('删除失败')
-        this.$message.success('删除成功')
-        this.getUsers()
-    }
+          type: 'warning',
+        }
+      ).catch((err) => err)
+      //如果结果为confirm则表示确定
+      //如果结果为cancel则表示取消
+      if (confirmResult !== 'confirm') return this.$message.info('取消删除')
+      const { data: res } = await this.$http.delete('users/' + id)
+      if (res.meta.status !== 200) return this.$message.error('删除失败')
+      this.$message.success('删除成功')
+      this.getUsers()
+    },
   },
-};
+}
 </script>
 <style lang="less" scoped>
 </style>
